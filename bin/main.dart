613397a145b256a11046857cebbcb0f86a1d8830
Mokhtar 2 <mokhtar_met@yahoo.fr>
2025-04-16 00:14:13 +0200

    write onto file

--- bin/main.dart
+++ bin/main.dart
@@ -1,13 +1,18 @@
 import 'dart:io';
 
 main(List<String> arguments) {
-
+  // configure directory path
   Directory dir = Directory.current;
+
+  // create file if not exists + empty its content
   File file = new File(dir.path + '/myfile.txt');
-
-  file.writeAsStringSync('Hello World');
-  print(file.readAsStringSync());
-
   file.deleteSync();
 
+  //write onto file
+  for(int i =0; i < 5; i++) {
+    file.writeAsStringSync('Hello World ${i+1}\n', mode: FileMode.append);
+
+    // Console content check
+    print('Hello World ${i+1}\n');
+  }
 }
